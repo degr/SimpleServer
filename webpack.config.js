b801f7c101c4ffaef8318b3049356d1434b2c362
rsmirnou <3Hjvfy0204>
2018-10-11 00:21:13 +0300

    babel & react npm scripts for watch and build

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'assets/js/'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     }
 };
